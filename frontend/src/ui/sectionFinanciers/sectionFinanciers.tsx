affc8bd544f9fe78ab67f2a875f3987e0025de79
Priscillia <selvapriscillia@yahoo.fr>
2025-06-30 10:25:18 +0200

    create section financiers

---
+++ frontend/src/ui/sectionFinanciers/sectionFinanciers.tsx
@@ -0,0 +1,48 @@
+import logoUE from "../../assets/UE.webp";
+import logoCharente from "../../assets/logoCharente.webp";
+import logoRepFrance from "../../assets/logo-de-la-republique-francaise.webp";
+import logoConvergence from "../../assets/convergence.webp";
+import "./sectionFinanciers.css";
+import SubSubtitle from "../subSubtitle/subSubtitle";
+
+const financeurs = [
+  {
+    src: logoUE,
+    alt: "Logo Co-financé par l'Union Européenne",
+    aria: "Logo Co-financé par l'Union Européenne",
+  },
+  {
+    src: logoCharente,
+    alt: "Logo Charente le département",
+    aria: "Logo Charente le département",
+  },
+  {
+    src: logoRepFrance,
+    alt: "Logo de la République Française",
+    aria: "Logo de la République Française",
+  },
+  {
+    src: logoConvergence,
+    alt: "Logo convergence",
+    aria: "Logo convergence",
+  },
+];
+
+export default function SectionFinanciers() {
+  return (
+    <section className="section-financeurs">
+      <SubSubtitle subSubtitle="Financeurs" />
+      <div className="logos-wrapper">
+        {financeurs.map((f, i) => (
+          <img
+            key={i}
+            src={f.src}
+            alt={f.alt}
+            aria-label={f.aria}
+            className="logo-financeurs"
+          />
+        ))}
+      </div>
+    </section>
+  );
+}
