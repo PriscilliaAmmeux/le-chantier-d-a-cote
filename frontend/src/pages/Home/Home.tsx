7ba3538f82a8df8f4afaa294ccb6a717556bc412
Priscillia <selvapriscillia@yahoo.fr>
2025-07-01 15:01:54 +0200

    create button and insert home page

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,4 +1,5 @@
 import DataMonth from "../../components/dataMonth/dataMonth";
+import Button from "../../ui/button/button";
 import Layout from "../../ui/layout/layout";
 import SectionFinanciers from "../../ui/sectionFinanciers/sectionFinanciers";
 import Title from "../../ui/title/title";
@@ -12,9 +13,15 @@ export default function Home() {
         <DataMonth />
       </section>
       <section>
-      
         <SectionFinanciers />
       </section>
+      <div className="button-center">
+        <Button
+          text={"En savoir plus"}
+          to={"/about"}
+          ariaLabel="En savoir plus"
+        />
+      </div>
     </Layout>
   );
 }
