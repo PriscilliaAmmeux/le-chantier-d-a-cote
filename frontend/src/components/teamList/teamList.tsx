042400d148ad9ef687676ef5b2ef983951d18948
Priscillia <selvapriscillia@yahoo.fr>
2025-07-03 20:30:17 +0200

    refacto

--- frontend/src/components/teamList/teamList.tsx
+++ frontend/src/components/teamList/teamList.tsx
@@ -1,54 +1,58 @@
-import "./teamSupervisors.css";
+import "./teamList.css";
 
-type Supervisor = {
+type TeamMember = {
   name: string;
   photo: string;
   presentation: string;
 };
 
-interface TeamSupervisorsProps {
-  supervisors: Supervisor[];
+interface TeamListProps {
+  members: TeamMember[];
 }
 
-export default function TeamSupervisors({ supervisors }: TeamSupervisorsProps) {
+export default function TeamList({ members }: TeamListProps) {
   return (
     <div className="team-supervisors-list">
-      {supervisors.map((sup, idx) => (
+      {members.map((member, idx) => (
         <div
           className={`supervisor-row ${
             idx % 2 === 0 ? "left-photo" : "right-photo"
           }`}
-          key={sup.name}>
+          key={member.name}>
           {idx % 2 === 0 ? (
             <>
               <img
-                src={`/images/team/${sup.photo}`}
-                alt={`Portrait de ${sup.name}`}
-                aria-label={`Portrait de ${sup.name}`}
+                src={`/images/team/${member.photo}`}
+                alt={`Portrait de ${member.name}`}
+                aria-label={`Portrait de ${member.name}`}
                 className="supervisor-photo"
                 loading="lazy"
                 width={120}
                 height={120}
               />
               <div className="supervisor-info">
-                <div className="supervisor-name font-bold">{sup.name}</div>
+                <div className="supervisor-name font-bold">{member.name}</div>
                 <div className="supervisor-presentation">
-                  {sup.presentation}
+                  {member.presentation}
                 </div>
               </div>
             </>
           ) : (
             <>
               <div className="supervisor-info">
-                <div className="supervisor-name font-bold">{sup.name}</div>
+                <div className="supervisor-name font-bold">{member.name}</div>
                 <div className="supervisor-presentation">
-                  {sup.presentation}
+                  {member.presentation}
                 </div>
               </div>
               <img
-                src={`/images/team/${sup.photo}`}
-                alt={sup.name}
+                src={`/images/team/${member.photo}`}
+                alt={`Portrait de ${member.name}`}
+                aria-label={`Portrait de ${member.name}`}
                 className="supervisor-photo"
+                loading="lazy"
+                width={120}
+                height={120}
               />
             </>
           )}
