242807be8e8b990aa3da200625cfc52046e19a75
Priscillia <selvapriscillia@yahoo.fr>
2025-07-09 11:32:34 +0200

    style css

--- frontend/src/pages/Activities/activities.tsx
+++ frontend/src/pages/Activities/activities.tsx
@@ -8,15 +8,20 @@ export default function Activities() {
   return (
     <Layout>
       <Title text="Activités proposées par Le Chantier d’À Côté" />
-      <p className="text-align-justify line-height margin-top">
+      <p className="text-align-justify line-height ">
         Notre association d’insertion sociale et professionnelle propose aux
         bénéficiaires divers ateliers et chantiers pour les former et les
         initier au marché du travail. Parmi nos principales activités pour le
         retour à l’emploi : l’entretien des espaces verts, la rénovation de
         bâtiments et les travaux viticoles.
       </p>
-      <GreenSpace />
-      <Building />
+      <section className="activity-section margin-top">
+        <GreenSpace />
+      </section>
+
+      <section className="activity-section">
+        <Building />
+      </section>
     </Layout>
   );
 }
