6dbc071a193cdc4b14fc59c06b5ee498c0b191f9
Priscillia <selvapriscillia@yahoo.fr>
2025-08-06 21:38:42 +0200

    feat: create section videos

--- frontend/src/pages/Missions/missions.tsx
+++ frontend/src/pages/Missions/missions.tsx
@@ -1,10 +1,31 @@
+import VideoSection from "../../components/videoSection/VideoSection";
 import Layout from "../../ui/layout/layout";
+import SubSubtitle from "../../ui/subSubtitle/subSubtitle";
+import Subtitle from "../../ui/subtitle/subtitle";
 import Title from "../../ui/title/title";
+import "./missions.css";
+
+const videosData = [
+  {
+    id: "Cw6kfgFLe68",
+    title: "Mon chantier d'insertion par le travail",
+    subtitle: "Mon chantier d'insertion par le travail",
+  },
+  {
+    id: "nQC-W7h7eyg",
+    title: "Présentation de l'Atelier Chantier d'Insertion",
+    subtitle: "Présentation de l'Atelier Chantier d'Insertion (ACI)",
+  },
+];
 
 export default function Missions() {
   return (
     <Layout>
       <Title text="Missions" />
+      <div className="missions-subtitle">
+        <Subtitle subtitle="Encadrement et Accompagnement Socioprofessionnel" />
+      </div>
+      <VideoSection videos={videosData} />
     </Layout>
   );
 }
