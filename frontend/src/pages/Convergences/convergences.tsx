9f039e1e3e55e449c44ccd5c4e56ee758a03c550
Priscillia <selvapriscillia@yahoo.fr>
2025-08-09 21:46:55 +0200

    add text convergences page + fix css mobile

--- frontend/src/pages/Convergences/convergences.tsx
+++ frontend/src/pages/Convergences/convergences.tsx
@@ -1,54 +1,25 @@
+import SectionConvergences from "../../ui/sectionConvergences/sectionConvergences";
 import Layout from "../../ui/layout/layout";
-import Subtitle from "../../ui/subtitle/subtitle";
 import Title from "../../ui/title/title";
 import "./convergences.css";
+import SectionFirstHours from "../../ui/sectionFirstHours/sectionFirstHours";
 
 export default function Convergences() {
   return (
     <Layout className="convergences">
       <Title text="Convergences" />
+      <SectionConvergences />
 
-      <section className="margin-top-subtitle">
-        <Subtitle subtitle="L'association Convergences France" />{" "}
+      <section className="data-section">
+        <div className="data-placeholder">
+          <h3 className="data-title">Données et chiffres clés</h3>
+          <p className="data-text">
+            Section en attente des statistiques officielles
+          </p>
+        </div>
       </section>
 
-      <p className="convergences-text">
-        Attente texte : Lorem ipsum dolor sit amet, consectetur adipisicing
-        elit. Alias placeat, officiis hic aperiam sed amet impedit, sapiente,
-        natus iste illo nobis laborum possimus eaque quae. Ullam enim eum alias
-        obcaecati. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-        Alias placeat, officiis hic aperiam sed amet impedit, sapiente, natus
-        iste illo nobis laborum possimus eaque quae. Ullam enim eum alias
-        obcaecati.Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-        Alias placeat, officiis hic aperiam sed amet impedit, sapiente, natus
-        iste illo nobis laborum possimus eaque quae. Ullam enim eum alias
-        obcaecati.Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-        Alias placeat, officiis hic aperiam sed amet impedit, sapiente, natus
-        iste illo nobis laborum possimus eaque quae. Ullam enim eum alias
-        obcaecati.
-      </p>
-
-      <section className="data-month-section">
-        <p>Attente chiffre</p>
-      </section>
-
-      <section className="section-firthours">
-        <Subtitle subtitle="Premières heures" />
-        <p className="convergences-text">
-          Attente texte : Lorem ipsum dolor sit amet, consectetur adipisicing
-          elit. Alias placeat, officiis hic aperiam sed amet impedit, sapiente,
-          natus iste illo nobis laborum possimus eaque quae. Ullam enim eum
-          alias obcaecati. Lorem ipsum dolor sit amet, consectetur adipisicing
-          elit. Alias placeat, officiis hic aperiam sed amet impedit, sapiente,
-          natus iste illo nobis laborum possimus eaque quae. Ullam enim eum
-          alias obcaecati. Lorem ipsum dolor sit amet, consectetur adipisicing
-          elit. Alias placeat, officiis hic aperiam sed amet impedit, sapiente,
-          natus iste illo nobis laborum possimus eaque quae. Ullam enim eum
-          alias obcaecati.
-        </p>
-      </section>
-
-      <section></section>
+      <SectionFirstHours />
     </Layout>
   );
 }
