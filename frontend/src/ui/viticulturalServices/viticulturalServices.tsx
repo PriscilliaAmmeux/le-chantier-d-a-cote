e2c9ff69b886fa0f770c460b16fb48b3656bbfd9
Priscillia <selvapriscillia@yahoo.fr>
2025-08-17 17:24:54 +0200

    replace img

--- frontend/src/ui/viticulturalServices/viticulturalServices.tsx
+++ frontend/src/ui/viticulturalServices/viticulturalServices.tsx
@@ -1,5 +1,5 @@
 import ActivityBlock from "../../components/activityBlock/activityBlock";
-import imgViticulture from "/img6.webp";
+import imgViticulture from "/viticole.webp";
 import SubSubtitle from "../subSubtitle/subSubtitle";
 import MissionsList from "../missionList/missionList";
 import "./viticulturalServices.css";
