6400697f5e31e71ad815d2caff9b07dee4e37e3f
Priscillia <selvapriscillia@yahoo.fr>
2025-10-08 20:35:27 +0200

    add logo ue

--- frontend/src/ui/header/header.tsx
+++ frontend/src/ui/header/header.tsx
@@ -1,4 +1,5 @@
 import logo from "/logo.webp";
+import logoUE from "/UE.webp";
 import NavBar from "../navBar/navBar";
 import NavBarMobile from "../navBarMobile/navBarMobile";
 import "./header.css";
@@ -6,12 +7,22 @@ import "./header.css";
 export default function Header() {
   return (
     <header className="header">
-      <img
-        src={logo}
-        alt="Logo du Chantier d'à Côté"
-        aria-label="Logo du Chantier d'à Côté"
-        className="header-logo"
-      />
+      <div className="header-logos">
+        <div className="header-logo-center">
+          <img
+            src={logo}
+            alt="Logo du Chantier d'à Côté"
+            aria-label="Logo du Chantier d'à Côté"
+            className="header-logo"
+          />
+        </div>
+        <img
+          src={logoUE}
+          alt="Logo cofinancé par l'Union Européenne"
+          aria-label="Logo cofinancé par l'Union Européenne"
+          className="header-logo-ue"
+        />
+      </div>
       <NavBar />
       <NavBarMobile />
     </header>
--- frontend/src/ui/interventionZones/interventionZones.tsx
+++ frontend/src/ui/interventionZones/interventionZones.tsx
@@ -6,7 +6,7 @@ export default function InterventionZones() {
   return (
     <SectionCard>
       <Subtitle subtitle="Nos zones d’intervention" />
-      <p className="">
+      <p className="intervention-line-height">
         Nous intervenons dans un rayon de 35 à 40 km autour de Val de Cognac,
         principalement sur le territoire de Grand Cognac, ainsi que dans les
         communes limitrophes du côté de Burie et Chérac (17).
